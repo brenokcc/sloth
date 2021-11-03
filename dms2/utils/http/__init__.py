a742a5706715b9640fdbe4ac356378d09e7e5f24
Carlos Breno Pereira Silva <breno.silva@ifrn.edu.br>
2021-11-02 21:15:34 -0300

    Continuação da implementação

---
+++ dms2/utils/http/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import xlwt
+import csv
+from tempfile import mktemp
+from django.http import HttpResponse
+
+
+class XlsResponse(HttpResponse):
+    def __init__(self, data):
+        wb = xlwt.Workbook(encoding='iso8859-1')
+        for title, rows in data:
+            sheet = wb.add_sheet(title)
+            for row_idx, row in enumerate(rows):
+                for col_idx, label in enumerate(row):
+                    sheet.write(row_idx, col_idx, label=label)
+        path = mktemp()
+        wb.save(path)
+        file = open(path, 'rb')
+        content = file.read()
+        file.close()
+        super().__init__(content=content, content_type='application/vnd.ms-excel')
+        self['Content-Disposition'] = 'attachment; filename=Download.xls'
+
+
+class CsvResponse(HttpResponse):
+    def __init__(self, rows):
+        path = mktemp()
+        with open(path, 'w', encoding='iso8859-1') as output:
+            writer = csv.writer(output)
+            for row in rows:
+                writer.writerow([col for col in row])
+        file = open(path, 'rb')
+        content = file.read()
+        file.close()
+        super().__init__(content=content, content_type='application/csv')
+        self['Content-Disposition'] = 'attachment; filename=Download.csv'

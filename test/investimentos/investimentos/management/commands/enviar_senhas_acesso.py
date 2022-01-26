14b9c10a544ef9b48d6a1d681705c304c804b91d
Breno Silva <brenokcc@yahoo.com.br>
2022-01-26 17:28:38 -0300

    Ajuste solicitado por Jessica

--- test/investimentos/investimentos/management/commands/enviar_senhas_acesso.py
+++ test/investimentos/investimentos/management/commands/enviar_senhas_acesso.py
@@ -5,7 +5,6 @@ from uuid import uuid1
 
 
 MENSAGEM = '''
-Assunto: [SETEC] Credenciais de Acesso – Sistema de Coleta de Demandas
 
 Senhor(a) gestor(a),
 
@@ -18,7 +17,7 @@ Senha: {senha}
 Por questões de segurança, orienta-se que no primeiro acesso seja feita a alteração da senha. Para isso, no canto superior direito do sistema, clique no e-mail da instituição e selecione a opção “Alterar Senha”. 
 
 Para acessar o tutorial do sistema em PDF, acesse https://coletasetec.ifrn.edu.br/tutorial/tutoria.pdf.
-Caso prefira o tutorial no formato de video, acesse https://coletasetec.ifrn.edu.br/videos/.
+Caso prefira o tutorial no formato de vídeo, acesse https://coletasetec.ifrn.edu.br/videos/.
 
 Diretoria de Desenvolvimento da Rede Federal
 Secretaria de Educação Profissional e Tecnológica
@@ -35,6 +34,7 @@ class Command(BaseCommand):
             gestor.user.set_password(senha)
             gestor.user.save()
             send_mail(
+                '[SETEC] Credenciais de Acesso – Sistema de Coleta de Demandas',
                 MENSAGEM.format(email=gestor.email, senha=senha),
                 [gestor.email],
                 fail_silently=False,

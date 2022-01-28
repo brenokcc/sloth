b76507a7cecb3cffd4bfff22e4de1ac478ec8cb5
Breno Silva <brenokcc@yahoo.com.br>
2022-01-28 15:44:14 -0300

    Comando para exibir senhas de acesso

---
+++ test/investimentos/investimentos/management/commands/exibir_senhas_acesso.py
@@ -0,0 +1,46 @@
+from django.core.management import BaseCommand
+from ...models import Gestor
+from uuid import uuid1
+from django.utils.html import strip_tags
+
+
+MENSAGEM = '''
+
+<b>Senhor(a) gestor(a)</b>,
+
+<p>Seguem as informações de acesso ao <b>Sistema de Coleta de Demandas da Rede Federal de Educação Profissional, Científica e Tecnológica</b>.</p>
+
+<p>
+<i>URL</i>: <a href="https://coletasetec.ifrn.edu.br/">https://coletasetec.ifrn.edu.br/</a> <br>
+<i>Login</i>: {email} <br>
+<i>Senha</i>: {senha} <br>
+</p>
+
+<p>Por questões de segurança, orienta-se que no primeiro acesso seja feita a alteração da senha. Para isso, no canto superior direito do sistema, clique no e-mail da instituição e selecione a opção “Alterar Senha”.</p>
+
+<p>Para acessar o tutorial do sistema em PDF, acesse <a href="https://coletasetec.ifrn.edu.br/media/tutorial/tutorial.pdf">https://coletasetec.ifrn.edu.br/media/tutorial/tutorial.pdf</a>.<br>
+Caso prefira o tutorial no formato de vídeo, acesse <a href="https://coletasetec.ifrn.edu.br/videos/">https://coletasetec.ifrn.edu.br/videos/</a>.</p>
+
+<p>
+<b>Diretoria de Desenvolvimento da Rede Federal<br>
+Secretaria de Educação Profissional e Tecnológica</b>
+</p>
+'''
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        qs = Gestor.objects.filter(user__last_login__isnull=True)
+        qs = qs.filter(pk__in=(9, 31, 5, 25))
+        for gestor in qs:
+            senha = uuid1().hex[0:3]
+            gestor.user.set_password(senha)
+            gestor.user.save()
+            subject = '[SETEC] Credenciais de Acesso – Sistema de Coleta de Demandas'
+            html_content = MENSAGEM.format(email=gestor.email, senha=senha)
+            text_content = strip_tags(html_content)
+            print('ASSUNTO:', subject)
+            print('E-MAIL', gestor.email)
+            print(text_content)
+            print('===== *** =====\n\n\n')
+

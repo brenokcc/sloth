ef880373ce07fa456a44ea7d70ac54c2f0d20a89
Breno Silva <brenokcc@yahoo.com.br>
2022-09-12 02:00:44 -0300

    0.0.33

--- test/erp/base/tests.py
+++ test/erp/base/tests.py
@@ -132,8 +132,9 @@ class Oauth2TestCase(ServerTestCase):
         self.login('admin', '123')
         data = dict(name='public', description='Publica Data')
         self.post('/api/scope/add/', data=data)
+        client_secret = 'zzGrrKWXFtlVG0baBUKZPRhClmPlrp3TYefnXY3RRlk3hZcPmm4xMF2iCMdG2MvcxTfvWKzEXNsB6bmIzdvSWpuJARBHaifut8tGoVyS7lPUZuiMtfvYr4A9hGNt8aEM'
         data = dict(
-            redirect_uris='', client_type='confidential',
+            redirect_uris='', client_type='confidential', client_secret=client_secret,
             authorization_grant_type='password', name='App01',
             skip_authorization=True, user=admin.pk, client_id=generate_client_id(),
             default_scopes=[1], available_scopes=[1]
@@ -145,7 +146,7 @@ class Oauth2TestCase(ServerTestCase):
 
         data = dict(
             client_id=app['access_data']['client_id'],
-            client_secret=app['access_data']['client_secret'],
+            client_secret=client_secret,
             grant_type=app['access_data']['authorization_grant_type'],
             username=admin.username,
             password='123',

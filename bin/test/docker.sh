0154349616d407c508661c9042968492b1f06f45
Breno Silva <brenokcc@yahoo.com.br>
2022-09-19 11:02:34 -0300

    Teste com docker

--- bin/test/docker.sh
+++ bin/test/docker.sh
@@ -1,2 +1,3 @@
 docker build --target sloth-test -t sloth-test .
 docker run --rm --name test-app -v $(pwd)/test/enderecos:/app -w /app sloth-test python manage.py test
+docker run --rm --name test-app -v $(pwd)/test/petshop:/app -w /app sloth-test python manage.py test

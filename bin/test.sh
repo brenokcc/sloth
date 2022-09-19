2897e9576a238f82fb80ac863fa8498b460201e0
Breno Silva <brenokcc@yahoo.com.br>
2022-09-19 12:08:44 -0300

    Teste com docker

--- bin/test.sh
+++ bin/test.sh
@@ -1,4 +1,4 @@
-# docker build --target sloth-test -t sloth-test .
+docker build --target sloth-test -t sloth-test .
 APPS=${1:-"erp papeis enderecos petshop"}
 for APP in $APPS; do
   docker run --rm --name test-app -v "$(pwd)/test/$APP:/app" -w /app sloth-test python manage.py test

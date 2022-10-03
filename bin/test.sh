9ad00af71e224e317640734818df5b15a06d204c
Breno Silva <brenokcc@yahoo.com.br>
2022-10-03 16:08:26 -0300

    Release pelo github

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,5 @@
 docker build --target sloth-test -t sloth-test .
 APPS=${1:-"erp papeis petshop"}
 for APP in $APPS; do
-  docker run --rm --name test-app -v "$(pwd)/test/$APP:/app" -w /app sloth-test python manage.py test
+  echo docker run --rm --name test-app -v "$(pwd)/test/$APP:/app" -w /app sloth-test python manage.py test
 done

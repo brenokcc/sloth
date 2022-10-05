2bcde4622a0701170fa0b852268ba06616f56a01
Breno Silva <brenokcc@yahoo.com.br>
2022-10-04 23:02:56 -0300

    0.0.39

--- bin/release/dockerhub.sh
+++ bin/release/dockerhub.sh
@@ -1,5 +1,19 @@
-VERSION=$(cat setup.py | grep version | sed "s/',//" | sed "s/.*='//")
-echo "Building brenokcc/sloth:$VERSION"
-docker build --target sloth -t brenokcc/sloth:$VERSION .
-docker login -u brenokcc -p $DOCKERHUB_PASSWORD
-docker push brenokcc/sloth:$VERSION
+CURRENT_VERSION=$(cat setup.py | grep version | sed "s/',//" | sed "s/.*='//")
+DOCKERHUB_VERSION=$(curl -s https://hub.docker.com/v2/repositories/brenokcc/sloth/tags/ | grep -o $CURRENT_VERSION)
+echo "Current version: $CURRENT_VERSION"
+echo "Dockerhub version: $DOCKERHUB_VERSION"
+if [[ "$DOCKERHUB_VERSION" != "$CURRENT_VERSION" ]]; then
+  echo "Building brenokcc/sloth:$VERSION"
+  PYPI_VERSION=$(pip install django-sloth==0.0.0 2>&1 >/dev/null | grep -o $CURRENT_VERSION)
+  echo "PyPi version: $PYPI_VERSION"
+  if [[ "$PYPI_VERSION" == "" ]]; then
+    sleep 60;
+  fi
+  PYPI_VERSION=$(pip install django-sloth==0.0.0 2>&1 >/dev/null | grep -o $CURRENT_VERSION)
+  echo "PyPi version: $PYPI_VERSION"
+  if [[ "$PYPI_VERSION" != "" ]]; then
+    docker build --target sloth -t brenokcc/sloth:$VERSION .
+    docker login -u brenokcc -p $DOCKERHUB_PASSWORD
+    docker push brenokcc/sloth:$VERSION
+  fi
+fi
--- bin/release/pypi.sh
+++ bin/release/pypi.sh
@@ -1,3 +1,7 @@
-pip install twine
-python setup.py sdist
-twine upload "dist/$(ls -rt dist | tail -1)" -u brenokcc -p $PIP_PASSWORD
+CURRENT_VERSION=$(cat setup.py | grep version | sed "s/',//" | sed "s/.*='//")
+REMOTE_VERSION=$(pip install django-sloth==0.0.0 2>&1 >/dev/null | grep -o $CURRENT_VERSION)
+if [[ "$REMOTE_VERSION" != "$CURRENT_VERSION" ]]; then
+  pip install twine
+  python setup.py sdist
+  twine upload "dist/$(ls -rt dist | tail -1)" -u brenokcc -p $PIP_PASSWORD
+fi

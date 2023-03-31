e7ef72ded7bafe4992315739a48a16e050a20058
Breno Silva <brenokcc@yahoo.com.br>
2023-03-31 19:25:46 -0300

    Continuação da implementação

--- test/coletasetec/investimentos/settings.py
+++ test/coletasetec/investimentos/settings.py
@@ -186,3 +186,8 @@ SLOTH = {
 
 #SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 #CACHES = {'default': {'BACKEND': 'django_redis.cache.RedisCache', 'LOCATION': 'redis://127.0.0.1:6379/1', 'OPTIONS': {'CLIENT_CLASS': 'django_redis.client.DefaultClient'}}}
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass

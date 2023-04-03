83bbd208f54b147e9f4503a372273d347cbe7a4f
Breno Silva <brenokcc@yahoo.com.br>
2023-04-02 22:47:21 -0300

    Continuação da implementação

--- test/coletasetec/investimentos/mail.py
+++ test/coletasetec/investimentos/mail.py
@@ -1,4 +1,3 @@
-from uuid import uuid1
 from django.contrib.auth.models import User
 from django.utils.html import strip_tags
 from django.core.mail import EmailMultiAlternatives
@@ -8,37 +7,41 @@ MENSAGEM = '''
 
 <b>Senhor(a) gestor(a)</b>,
 
-<p>Seguem as informações de acesso ao <b>Sistema de Coleta de Demandas da Rede Federal de Educação Profissional, Científica e Tecnológica</b>.</p>
+<p>Seguem as informações de acesso ao Sistema de Coleta de Demandas da Rede Federal de Educação Profissional, Científica e Tecnológica – Exercício 2023.</p>
 
-<p>
-<i>URL</i>: <a href="https://coletasetec.ifrn.edu.br/">https://coletasetec.ifrn.edu.br/</a> <br>
-<i>Login</i>: {email} <br>
-<i>Senha</i>: {senha} <br>
+<p>URL: https://coletasetec.ifrn.edu.br/<br>
+Login: {email}<br>
+Senha: {senha}<br>
 </p>
 
-<p>Por questões de segurança, orienta-se que no primeiro acesso seja feita a alteração da senha. Para isso, no canto superior direito do sistema, clique no e-mail da instituição e selecione a opção “Alterar Senha”.</p>
+<p>Por questões de segurança, orienta-se que no primeiro acesso seja feita a alteração da senha. Para isso, no canto superior direito, clique nos três pontos verticais e selecione a opção “Alterar Senha”.</p>
 
-<p>Para acessar o tutorial do sistema em PDF, acesse <a href="https://coletasetec.ifrn.edu.br/media/tutorial/tutorial.pdf">https://coletasetec.ifrn.edu.br/media/tutorial/tutorial.pdf</a>.<br>
-Caso prefira o tutorial no formato de vídeo, acesse <a href="https://coletasetec.ifrn.edu.br/videos/">https://coletasetec.ifrn.edu.br/videos/</a>.</p>
+<p>O tutorial do sistema estará disponível em https://coletasetec.ifrn.edu.br/media/tutorial/tutorial.pdf. a partir das <b>16h (horário de Brasília) do dia 03/04/2023</b>. Não deixe de acessá-lo, pois foram realizadas várias atualizações em relação à versão anterior do sistema, para aprimorar e facilitar esse processo de coleta.<p>
+
+<p>Por fim, informamos, também, que nessa mesma data e horário o Ciclo de Demandas 2023 estará disponível para preenchimento das instituições.</p>
+
+<p>Bom trabalho!</p>
 
 <p>
-<b>Diretoria de Desenvolvimento da Rede Federal<br>
-Secretaria de Educação Profissional e Tecnológica</b>
+Diretoria de Desenvolvimento da Rede Federal de EPCT<br>
+Secretaria de Educação Profissional e Tecnológica<br>
+Ministério da Educação
 </p>
 '''
 
 
 def enviar_senha(gestor):
-    senha = '123' or uuid1().hex[0:3]
+    senha = 'pass{}'.format(gestor.pk)
     user = User.objects.get(username=gestor.email)
     user.set_password(senha)
     user.save()
-    subject = '[SETEC] Credenciais de Acesso – Sistema de Coleta de Demandas'
+    subject = '[SETEC] Credenciais de Acesso – Sistema de Coleta de Demandas 2023'
     html_content = MENSAGEM.format(email=gestor.email, senha=senha)
     text_content = strip_tags(html_content)
     from_email = 'naoresponder.ifrn.edu.br'
     to = gestor.email
+    to = 'brenokcc@yahoo.com.br'
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
     msg.attach_alternative(html_content, "text/html")
-    # msg.send()
+    msg.send()
     print(html_content)

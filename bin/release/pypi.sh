02ecadc8bb88f4d08d5f220d943a92aa49f53ccb
Breno Silva <brenokcc@yahoo.com.br>
2023-04-18 18:51:41 -0300

    Continuação da implementação

--- bin/release/pypi.sh
+++ bin/release/pypi.sh
@@ -1,5 +1,5 @@
 CURRENT_VERSION=$(cat setup.py | grep version | sed "s/',//" | sed "s/.*='//")
-REMOTE_VERSION=$(pip install django-sloth==0.0.0 2>&1 >/dev/null | grep -o $CURRENT_VERSION)
+REMOTE_VERSION=$(pip install sloth-framework==0.0.0 2>&1 >/dev/null | grep -o $CURRENT_VERSION)
 echo $REMOTE_VERSION $CURRENT_VERSION
 if [[ "$REMOTE_VERSION" != "$CURRENT_VERSION" ]]; then
   pip install twine
